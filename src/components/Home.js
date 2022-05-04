37d66887c0389de4010729f87885b0d9a730e487
lucianoarmoa98 <mmannuarmoa@gmail.com>
2022-05-04 19:40:35 -0400

    agregue iframe

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,14 +24,7 @@ const Home = () => {
 
     }, []);
 
-    useEffect(() => {
-        window.addEventListener("load", function () {
-            setTimeout(function () {
-                // This hides the address bar:
-                window.scrollTo(0, 1);
-            }, 0);
-        });
-    }, []);
+    
 
     console.log("dataState", dataDexie)
 
