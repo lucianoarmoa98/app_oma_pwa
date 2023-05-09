1754d102fa0e117f1293d3b79ff58a0022d7e344
Luciano Armoa <lucianoarmoa@MacBook-Air-de-Luciano.local>
2023-05-09 15:13:04 -0400

    se elimino dexie

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ProtectedRoutes, ProtectedRoutesLogin } from './components/Autorizador';
 import Home from './components/Home';
-import SesionUrl from './components/SesionUrl';
 import LoginScreen from './page/login/LoginScreen';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<ProtectedRoutesLogin />}>
-          {/* <Route path="/" element={<SesionUrl />} /> */}
           <Route path="/" element={<LoginScreen />} />
         </Route>
 
--- src/components/db.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('myDatabase');
-db.version(1).stores({
-  url: '++id, direccion', // Primary key and indexed props
-});
